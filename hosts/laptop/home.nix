127bf81c440c31f73004dfced3ef0ede3f2efdb2
Bigschniff <95365666+Onuphrius@users.noreply.github.com>
2026-03-11 19:53:54 +0100

    Create home.nix

---
+++ hosts/laptop/home.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, wallpapers, ... }:
+{
+  home.username = "lsd";
+  home.homeDirectory = "/home/lsd";
+  home.packages = with pkgs; [
+
+    # Utitlity
+    fastfetch
+    git
+    file
+    which
+    tree
+
+    # TUI
+    nnn
+    btop
+
+    # Language Servers
+    pyright
+    rust-analyzer
+    nil
+    nixfmt
+    
+    #Font
+    nerd-fonts.zed-mono
+  ];
+  
+  imports = [
+    ../../modules/programs/sway.nix
+    ../../modules/programs/waybar.nix
+    ../../modules/programs/kitty.nix
+    ../../modules/programs/rofi.nix
+    ../../modules/programs/neovim.nix
+    ../../modules/programs/librewolf.nix
+    ../../modules/programs/git.nix
+  ];
+
+  #Wallpapers
+  xdg.dataFile."wallpapers".source = wallpapers;
+  home.stateVersion = "25.11";
+}
