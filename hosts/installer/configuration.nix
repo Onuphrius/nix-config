d7a80cde3ca8f64f4a5cde833d9697eff7259f2d
Onuphrius <lukatowu@gmail.com>
2026-03-23 18:59:34 +0100

    Some tweaks

--- hosts/installer/configuration.nix
+++ hosts/installer/configuration.nix
@@ -6,6 +6,11 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwjaEC3NMfbKZ+YTTmcjUBUiqRLZZZJz8vuaODEmE8F bigschiff@pc"
   ];
   users.users.root.initialPassword = "root";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  script = "
+  #Disko command
+  sudo nixos-generate-config --no-filesystems --root /mnt
+  ";
 
 }
 
---
+++ hosts/thinkcentre1/configuration.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+
+{
+  imports = [
+    ./hardware.nix
+    ./disko.nix
+  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "cocaine";
+  networking.networkmanager.enable = true;
+
+  # Timezone
+  time.timeZone = "Europe/Berlin";
+
+  # Internationalisation
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+  console.keyMap = "de-latin1-nodeadkeys";
+
+  # Users
+  users.users.cocaine = {
+    isNormalUser = true;
+    description = "cocaine";
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwjaEC3NMfbKZ+YTTmcjUBUiqRLZZZJz8vuaODEmE8F bigschiff@pc"
+    ];
+  };
+  
+  security.polkit.enable = true;
+
+  # SSH
+  services.openssh = {                                                                       
+    enable = true;                                                                  
+    settings.PasswordAuthentication = false;                                                 
+  };
+
+  services.dbus.enable = true;
+  
+  # System version
+  system.stateVersion = "25.11";
+}
