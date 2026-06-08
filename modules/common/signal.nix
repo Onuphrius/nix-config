cd78fe14f1374fc2db3386fb5872118123b418aa
Onuphrius <lukatowu@gmail.com>
2026-06-09 01:38:46 +0200

    Added a toggle for wlsunset

--- modules/common/signal.nix
+++ modules/common/signal.nix
@@ -4,18 +4,25 @@
     signal-desktop
   ]);
 
-  #home.file.".config/Signal/ephemeral.json".text = 
-#  ''
-#{
-#  "localeOverride": null,
-#  "system-tray-setting": "DoNotUseSystemTray",
-#  "spell-check": true,
-#  "theme-setting": "dark",
-#  "window": {
-#    "autoHideMenuBar": true,
-#  }
-#}
-#
-#'';
+  home.file.".config/Signal/ephemeral.json".force = true;
+  
+  home.file.".config/Signal/ephemeral.json".text = 
+  ''
+{
+  "localeOverride": null,
+  "system-tray-setting": "DoNotUseSystemTray",
+  "spell-check": true,
+  "theme-setting": "dark",
+  "window": {
+    "maximized": false,
+    "autoHideMenuBar": false,
+    "fullscreen": false,
+    "width": 1896,
+    "height": 1013,
+    "x": 0,
+    "y": 0
+  }
+}
+  '';
 
 }
